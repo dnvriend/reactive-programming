8a42210a831a1d1b6cfbed4a466767e4a3a0e12b
dnvriend <dnvriend@gmail.com>
2015-04-20 18:16:20 +0200

    there is more to say about functions

--- src/test/scala/com/test/week1/FunctionTest.scala
+++ src/test/scala/com/test/week1/FunctionTest.scala
@@ -55,33 +55,108 @@ class FunctionTest extends TestSpec {
     val f6_3: (Int, Int, Int) => Unit = (x: Int, y: Int, z: Int) => Unit
   }
 
-  "A function" should "be applied" in {
+  "Functions" should "be able to be applied" in {
+    // A function, like the ones above, can be applied. What does that mean?
+    // A function like val `f = (x: Int) => x` can be evaluated. This
+    // can be done by applying a value to the function.
+
+    // when you evaluate a function by applying a value to that function
+    // it looks a lot like math:
+
+    val f: Int => Int = (x: Int) => x
+    f(3) shouldBe 3
+
+    // you can skip the type part if you wish:
+
+    val g = (x: Int) => x
+    g(3) shouldBe 3
+  }
+
+  it should "look like math" in {
+    val f = (x: Int) => x * x
+    f(2) shouldBe 4
+
+    val g = (x: Int) => x + x
+    g(2) shouldBe 4
+  }
+
+  it should "be applied in sequence" in {
+    val f: Int => Int = (x: Int) => x * x
+    val g: Int => Int = (x: Int) => x + x + x
+    val h: Int => Int = (x: Int) => g(f(x))
+    val i: Int => Int = f andThen g
+    val j: Int => Int = f compose g
+
+    // functions can be nested, this infuences the sequence of
+    // evaluation which is first 'f' andThen 'g':
+    g(f(2)) shouldBe 12
+
+    // the function 'h' does the same as the evaluation on line 91,
+    // but it is defined as a function, thus we can easily evaluate it
+    // by applying a number to it, like 2:
+    h(2) shouldBe 12
+
+    // the function 'i' composes these two functions. `andThen` is actually
+    // a method on the 'f' function object, which is of type Function1. This composition
+    // will first evaluate f and then evaluate g with the result of 'f', in which it does
+    // the same as line 96:
+    i(2) shouldBe 12
+
+    // the evaluation can also be reversed by first evaluating g and then f which is what
+    // function 'j' does:
+    j(2) shouldBe 36
+  }
+
+  //
+  // The functions above are always defined, which means they always give an answer,
+  // but there are functions, that can also be applied with a value, but only evaluate
+  // succesfully for certain inputs, let's look at some.
+  //
+
+  "A function" should "can be true for certain inputs" in {
     // the literal String => String is a Function1[String, String]
-    // this type can only be applied, because it is a Function,
-    // but, when the function is not defined, scala thows a match error
+    // this type can be applied, because it is a Function,
+    // but, when the function is not defined, scala thows a match error.
+    // for example, the function only has an answer for the input "ping", in
+    // which it will return a "pong", but for all other inputs, it will throw a
+    // 'Match Error'
     val f: String => String = { case "ping" => "pong" }
     f("ping") shouldBe "pong"
     intercept[MatchError] {
+      // evaluate the function by applying 'foo' to it
       f("foo")
     }
   }
 
+  // functions that throw 'MatchErrors' are a pain, wouldn't it be nice to first check
+  // whether or not a certain value can be applied to the function? Let's introduce the
+  // PartialFunction type, which is a subtype of Function!
+
   "A partial function" should "be applied" in {
-    // there is no partial function literal
-    // the partial function has the isDefinedAt method
+    // Note, there is no partial function literal in Scala. Scala has only
+    // the literal '=>' which is shorthand for Function.
+    // A partial function has the isDefinedAt method
     // so it can be checked
     val f: PartialFunction[String, String] = { case "ping" => "pong" }
+    // we can check whether the PartialFunction is defined for a given input
+    // which is a big plus!
     f.isDefinedAt("ping") shouldBe true
     f.isDefinedAt("foo") shouldBe false
     // it still throws a match error when applied though...
     intercept[MatchError] {
+      // evaluate the function by applying 'foo' to it
       f("foo")
     }
   }
 
+  //
+  // In Scala, collections are also functions, and therefor they can be evaluated:
+  //
+
   "A map" should "be a function" in {
     // a map is a function, thus is should be able to be applied
     val map = Map("1" -> "Foo", "2" -> "Bar")
+    // apply '1' and '2' to the function map (strange concept coming from Java)
     map("1") shouldBe "Foo"
     map("2") shouldBe "Bar"
     intercept[NoSuchElementException] {
@@ -100,21 +175,30 @@ class FunctionTest extends TestSpec {
   }
 
   "More complex partial function" should "be applied" in {
+    // 'f' is a PartialFunction that can be applied with 'List[Int]' and
+    // returns a String.
     val f: PartialFunction[List[Int], String] = {
       case Nil => "empty"
       case first :: second :: tail => s"$first::$second::$tail"
     }
 
-    // f is now a PartialFunction, which can be applied with a List[Int], lets do so
+    // As stated before, the PartialFunction can be applied with a List[Int], lets do so
     f(Nil) shouldBe "empty"
     f(List(1, 2, 3)) shouldBe "1::2::List(3)"
     f(List(1, 2, 3, 4)) shouldBe "1::2::List(3, 4)"
+    f.isDefinedAt(Nil) shouldBe true
+    f.isDefinedAt(List(1)) shouldBe false
+    f.isDefinedAt(List(1, 2)) shouldBe true
   }
 
   "Nested matching" should "be applied" in {
+    // 'f' is a PartialFunction that can be applied with 'List[Int]' and
+    // returns a String. Note the !!nested PartialFunction!!, which is only defined
+    // when the tail is 'Nil', but when the tail is eg. a List(1, 2, 3) the nested
+    // PartialFunction is not defined
     val f: PartialFunction[List[Int], String] = {
       case Nil => "empty"
-      case head :: tail => tail match {
+      case head :: tail => tail match { // nested PartialFunction
         case Nil => "empty"
       }
     }
