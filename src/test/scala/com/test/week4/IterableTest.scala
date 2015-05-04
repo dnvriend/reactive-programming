afe0a4ab7797e39a0492521aa570f1f454e2ad33
Dennis Vriend <dnvriend@gmail.com>
2015-05-04 22:31:23 +0200

    iterator test

---
+++ src/test/scala/com/test/week4/IterableTest.scala
@@ -0,0 +1,48 @@
+package com.test.week4
+
+import com.test.TestSpec
+
+import scala.collection.immutable.TreeSet
+
+class IterableTest extends TestSpec {
+
+  "List" should "be an Iterable" in {
+    List(1, 2, 3) shouldBe an [Iterable[_]]
+  }
+
+  it should "have next elements" in {
+    List(1, 2, 3).iterator should have ('hasNext(true))
+  }
+
+  "Vector" should "be an Iterable" in {
+    Vector(1, 2, 3) shouldBe an [Iterable[_]]
+  }
+
+  it should "have next elements" in {
+    Vector(1, 2, 3).iterator should have ('hasNext(true))
+  }
+
+  "Set" should "be an Iterable" in {
+    Set(1, 2, 3) shouldBe an [Iterable[_]]
+  }
+
+  it should "have next elements" in {
+    Set(1, 2, 3).iterator should have ('hasNext(true))
+  }
+
+  "Tree" should "be an Iterable" in {
+    TreeSet(1, 2, 3) shouldBe an [Iterable[_]]
+  }
+
+  it should "have next elements" in {
+    TreeSet(1, 2, 3).iterator should have ('hasNext(true))
+  }
+
+  "Map" should "be an Iterable" in {
+    Map(1 -> "1", 2 -> "2", 3 -> "3") shouldBe an [Iterable[_]]
+  }
+
+  it should "have next elements" in {
+    Map(1 -> "1", 2 -> "2", 3 -> "3").iterator should have ('hasNext(true))
+  }
+}
