5edba3626a42309a991efba772901735a59b9811
Dennis Vriend <dennis.vriend@trivento.nl>
2015-05-20 17:59:54 +0200

    supervision

--- src/test/scala/com/test/week6/SupervisionTest.scala
+++ src/test/scala/com/test/week6/SupervisionTest.scala
@@ -6,15 +6,23 @@ import akka.event.LoggingReceive
 import akka.testkit.TestProbe
 import com.test.TestSpec
 import scala.concurrent.duration._
+import akka.pattern.ask
 
 class SupervisionTest extends TestSpec {
 
   case class Command(f: () => Unit)
+  case object Count
+  case object GetState
+  case class CounterState(counter: Long)
 
   class Supervisor(tp: TestProbe, svs: SupervisorStrategy) extends Actor {
     val worker: ActorRef = context.actorOf(Props(new Actor with ActorLogging {
+      var counter = 0L
+
       override def receive: Receive = LoggingReceive {
         case Command(f) => f()
+        case Count => counter += 1
+        case GetState => sender() ! CounterState(counter)
       }
 
       override def preStart(): Unit =
@@ -35,25 +43,43 @@ class SupervisionTest extends TestSpec {
   def createSupervisor(tp: TestProbe)(svs: SupervisorStrategy) =
     system.actorOf(Props(new Supervisor(tp, svs)), s"sup-${randomId.take(3)}")
 
-  "SupervisorStrategy" should "resume the worker" in {
+  "SupervisorStrategy" should "resume the worker, state should not change, so should be 1" in {
     val tp = probe
     val sup = createSupervisor(tp) { OneForOneStrategy() {
         case t: RuntimeException => Resume
       }
     }
-    sup ! Command (() => throw new RuntimeException("stop"))
-    tp.expectNoMsg(1.second)
+    sup ! Count
+    (sup ? GetState).futureValue shouldBe CounterState(1L)
+    sup ! Command (() => throw new RuntimeException("resume"))
+    (sup ? GetState).futureValue shouldBe CounterState(1L)
+    tp.expectNoMsg(100.millis) // no Terminated message
     cleanup(sup)
   }
 
-  it should "stop the worker" in {
+
+  it should "restart the worker, so the worker instance has been replaced, and state should be 0 again" in {
+    val tp = probe
+    val sup = createSupervisor(tp) { OneForOneStrategy() {
+      case t: RuntimeException => Restart
+    }
+    }
+    sup ! Count
+    (sup ? GetState).futureValue shouldBe CounterState(1L)
+    sup ! Command (() => throw new RuntimeException("restart"))
+    (sup ? GetState).futureValue shouldBe CounterState(0L)
+    tp.expectNoMsg(100.millis) // no Terminated message
+    cleanup(sup)
+  }
+
+  it should "stop the worker, so worker in not there anymore and should not answer" in {
     val tp = probe
     val sup = createSupervisor(tp) { OneForOneStrategy() {
         case t: RuntimeException => Stop
       }
     }
     sup ! Command (() => throw new RuntimeException("stop"))
-    tp.expectMsgPF[Unit](1.second) {
+    tp.expectMsgPF[Unit](100.millis) {
       case Terminated(_) =>
     }
     cleanup(sup)
